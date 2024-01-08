339b3bf33f6e35f1e0f9adc7c7dc113590dd5b0c
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-01-07 19:02:19 -0700

    CYA

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,9 +10,9 @@ export const Footer = () => {
       style={{ height: "8vh", borderTop: "1px solid #eeeeee" }}
     >
       <Container className="text-center text-secondary">
-        Your data is saved in your browser in "local storage", not on our
-        servers (we don't have any!). Clearing browser cookies will likely clear
-        "local storage" too.
+        This is a proof-of-concept. Your data is saved in your browser in "local
+        storage", not on our servers (we don't have any!). Clearing browser
+        cookies will likely clear "local storage" too.
       </Container>
       <Container className="text-center">
         <a
