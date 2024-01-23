110217ea4347afb03b5a4239b5beee4f33b407be
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-01-23 14:55:22 -0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Header } from "./components/Header";
 import { PlanContainer } from "./pages/plan/PlanContainer";
-import { Footer } from "./components/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "react-error-boundary";
