0f7346e43725d6dc55a69111879804aa15d20f09
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-02-13 18:53:00 -0700

    refactor: cleanup exceptions inputs

---
+++ src/pages/plan/rules/AddEditRule/Exceptions.tsx
@@ -0,0 +1,146 @@
+import BSForm from "react-bootstrap/esm/Form";
+import { FieldArray, useFormikContext } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Accordion from "react-bootstrap/Accordion";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
+import InputGroup from "react-bootstrap/esm/InputGroup";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { IParameters } from "../../../../services/ParameterService";
+import Button from "react-bootstrap/esm/Button";
+
+function DateAdder({
+  onAdd,
+  min,
+}: {
+  onAdd: (date: string) => void;
+  min: string;
+}) {
+  const [value, setValue] = useState("");
+  return (
+    <InputGroup>
+      <BSForm.Control
+        type="date"
+        min={min}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />{" "}
+      <InputGroup.Text>
+        <FontAwesomeIcon
+          title="Add"
+          icon={faPlus}
+          style={{
+            color: "var(--primary)",
+          }}
+          onClick={() => {
+            onAdd(value);
+          }}
+        />
+      </InputGroup.Text>
+    </InputGroup>
+  );
+}
+
+export function Exceptions({ parameters }: { parameters: IParameters }) {
+  const { startDate } = parameters;
+
+  const form = useFormikContext();
+
+  const exdates = form.getFieldMeta("rrule.exdates").value as string[];
+  const rdates = form.getFieldMeta("rrule.rdates").value as string[];
+
+  const [showExceptions, setShowExceptions] = useState(false);
+
+  return (
+    <>
+      <Button
+        variant="link"
+        className="p-0 m-0"
+        style={{
+          color: "var(--tertiary)",
+          textDecoration: "none",
+        }}
+        onClick={() => {
+          setShowExceptions((s) => !s);
+        }}
+      >
+        {showExceptions ? (
+          <>Hide Exceptions ({exdates.length + rdates.length})</>
+        ) : (
+          <>Show Exceptions ({exdates.length + rdates.length})</>
+        )}
+      </Button>
+
+      {showExceptions ? (
+        <Accordion flush>
+          <FieldArray name="rrule.exdates">
+            {(exdatesArrayHelpers) => (
+              <Accordion.Item eventKey="1">
+                <Accordion.Button className="p-1">
+                  Exclusions ({exdates.length})
+                </Accordion.Button>
+                <Accordion.Body>
+                  <ul>
+                    {exdates.sort().map((exdate, index) => (
+                      <li>
+                        {exdate}{" "}
+                        <FontAwesomeIcon
+                          style={{
+                            marginLeft: 10,
+                            color: "var(--red)",
+                          }}
+                          icon={faTrashCan}
+                          title="Delete"
+                          onClick={() => {
+                            exdatesArrayHelpers.remove(index);
+                          }}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                  <DateAdder
+                    min={startDate}
+                    onAdd={(d) => exdatesArrayHelpers.push(d)}
+                  />
+                </Accordion.Body>
+              </Accordion.Item>
+            )}
+          </FieldArray>
+          <FieldArray name="rrule.rdates">
+            {(rdatesArrayHelpers) => (
+              <Accordion.Item eventKey="2">
+                <Accordion.Button className="p-1">
+                  Inclusions ({rdates.length})
+                </Accordion.Button>
+                <Accordion.Body>
+                  <ul>
+                    {rdates.sort().map((rdate, index) => (
+                      <li>
+                        {rdate}{" "}
+                        <FontAwesomeIcon
+                          style={{
+                            marginLeft: 10,
+                            color: "var(--red)",
+                          }}
+                          icon={faTrashCan}
+                          title="Delete"
+                          onClick={() => {
+                            rdatesArrayHelpers.remove(index);
+                          }}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                  <DateAdder
+                    min={startDate}
+                    onAdd={(d) => rdatesArrayHelpers.push(d)}
+                  />
+                </Accordion.Body>
+              </Accordion.Item>
+            )}
+          </FieldArray>
+        </Accordion>
+      ) : null}
+    </>
+  );
+}
