2ff0197dd6fec5edae9d27a6e31920d01464a0b3
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-03-11 21:14:57 -0600

    feat: PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import posthog from "posthog-js";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./main.css";
+
+posthog.init("phc_Vgmc1Gq9hqRmpV8GKsjEJXLGvVflRjO4SOBnDUrp8BD", {
+  api_host: "https://app.posthog.com",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
