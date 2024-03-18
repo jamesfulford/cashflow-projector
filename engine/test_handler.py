178691d729f03a7d822bb2e5cdd57cc16529bce6
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-03-18 13:16:42 -0600

    test: cleanup some

--- engine/test_handler.py
+++ engine/test_handler.py
@@ -1,6 +1,6 @@
 import unittest
 from datetime import date
-from dateutil.rrule import rrule, MONTHLY, YEARLY, WEEKLY, DAILY, MO
+from dateutil.rrule import rrule, YEARLY
 from .exe_context import ExecutionParameters, ExecutionRules, ExecutionContext
 from .generate_instances import get_transactions_up_to
 from .daybydays import generate_daybydays
@@ -30,73 +30,85 @@ def get_rules_from_inputs(inputs):
             "value": rule["value"]
         }
     return ExecutionRules(rulemap)
+
+def get_inputs(i):
+    with open(f'engine/test-data/{i}/inputs.json') as f:
+        return json.load(f)
+
+def get_expected_transactions(i):
+    with open(f'engine/test-data/{i}/transactions.json') as f:
+        return json.load(f)
+    
+def get_expected_daybydays(i):
+    with open(f'engine/test-data/{i}/daybydays.json') as f:
+        return json.load(f)
     
 class HandlerTests(unittest.TestCase):
     def setUp(self):
         self.maxDiff = 5000
 
     def test_get_instances_from_rules_monthly(self):
-        inputs = json.load(open('engine/test-data/1/inputs.json'))
+        inputs = get_inputs(1)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/1/transactions.json'))
+        expected = get_expected_transactions(1)
         self.assertEqual(expected, actual)
 
     def test_get_instances_from_rules_multipleRules(self):
-        inputs = json.load(open('engine/test-data/2/inputs.json'))
+        inputs = get_inputs(2)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/2/transactions.json'))
+        expected = get_expected_transactions(2)
         self.assertEqual(expected, actual)
     
     def test_get_instances_from_rules_weekly(self):
-        inputs = json.load(open('engine/test-data/3/inputs.json'))
+        inputs = get_inputs(3)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/3/transactions.json'))
+        expected = get_expected_transactions(3)
         self.assertEqual(expected, actual)
     
 
     def test_get_instances_from_rules_biweekly(self):
-        inputs = json.load(open('engine/test-data/4/inputs.json'))
+        inputs = get_inputs(4)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/4/transactions.json'))
+        expected = get_expected_transactions(4)
         self.assertEqual(expected, actual)
 
     def test_get_instances_from_rules_yearly(self):
-        inputs = json.load(open('engine/test-data/5/inputs.json'))
+        inputs = get_inputs(5)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/5/transactions.json'))
+        expected = get_expected_transactions(5)
         self.assertEqual(expected, actual)
     
     def test_get_instances_from_rules_once(self):
-        inputs = json.load(open('engine/test-data/6/inputs.json'))
+        inputs = get_inputs(6)
         actual = get_transactions(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/6/transactions.json'))
+        expected = get_expected_transactions(6)
         self.assertEqual(expected, actual)
 
     def test_get_daybyday_from_rules_once(self):
-        inputs = json.load(open('engine/test-data/7/inputs.json'))
+        inputs = get_inputs(7)
         actual = get_daybyday(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/7/daybydays.json'))
+        expected = get_expected_daybydays(7)
         self.assertEqual(expected, actual)
     
 
@@ -119,39 +131,39 @@ class HandlerTests(unittest.TestCase):
 
 
     def test_get_daybyday_from_rules_multipleRules(self):
-        inputs = json.load(open('engine/test-data/8/inputs.json'))
+        inputs = get_inputs(8)
         actual = get_daybyday(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/8/daybydays.json'))
+        expected = get_expected_daybydays(8)
         self.assertEqual(expected, actual)
 
     def test_get_daybyday_from_rules_weekly(self):
-        inputs = json.load(open('engine/test-data/9/inputs.json'))
+        inputs = get_inputs(9)
         actual = get_daybyday(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/9/daybydays.json'))
+        expected = get_expected_daybydays(9)
         self.assertEqual(expected, actual)
 
 
     def test_get_daybyday_from_rules_daily(self):
-        inputs = json.load(open('engine/test-data/10/inputs.json'))
+        inputs = get_inputs(10)
         actual = get_daybyday(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/10/daybydays.json'))
+        expected = get_expected_daybydays(10)
         self.assertEqual(expected, actual)
 
 
     def test_get_daybyday_from_rules_monthly(self):
-        inputs = json.load(open('engine/test-data/11/inputs.json'))
+        inputs = get_inputs(11)
         actual = get_daybyday(
             get_parameters_from_inputs(inputs),
             get_rules_from_inputs(inputs)
         )
-        expected = json.load(open('engine/test-data/11/daybydays.json'))
+        expected = get_expected_daybydays(11)
         self.assertEqual(expected, actual)
