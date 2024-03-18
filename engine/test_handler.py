15ef6b410d7a4076666ba0db6cdab5f821214c93
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-03-18 15:53:12 -0600

    even more test improvements

--- engine/test_handler.py
+++ engine/test_handler.py
@@ -45,75 +45,47 @@ def get_expected_daybydays(i):
     
 class HandlerTests(unittest.TestCase):
     def setUp(self):
-        self.maxDiff = 5000
+        self.maxDiff = None
+    
+    def assert_transactions(self, i):
+        inputs = get_inputs(i)
+        self.assertEqual(get_expected_transactions(i), get_transactions(
+            get_parameters_from_inputs(inputs),
+            get_rules_from_inputs(inputs)
+        ))
+    
+    def assert_daybydays(self, i):
+        inputs = get_inputs(i)
+        self.assertEqual(get_expected_daybydays(i), get_daybyday(
+            get_parameters_from_inputs(inputs),
+            get_rules_from_inputs(inputs)
+        ))
 
     def test_get_instances_from_rules_monthly(self):
-        inputs = get_inputs(1)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(1)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(1)
+        self.assert_daybydays(1)
 
     def test_get_instances_from_rules_multipleRules(self):
-        inputs = get_inputs(2)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(2)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(2)
     
     def test_get_instances_from_rules_weekly(self):
-        inputs = get_inputs(3)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(3)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(3)
     
 
     def test_get_instances_from_rules_biweekly(self):
-        inputs = get_inputs(4)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(4)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(4)
 
     def test_get_instances_from_rules_yearly(self):
-        inputs = get_inputs(5)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(5)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(5)
     
     def test_get_instances_from_rules_once(self):
-        inputs = get_inputs(6)
-        actual = get_transactions(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_transactions(6)
-        self.assertEqual(expected, actual)
+        self.assert_transactions(6)
 
     def test_get_daybyday_from_rules_once(self):
-        inputs = get_inputs(7)
-        actual = get_daybyday(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_daybydays(7)
-        self.assertEqual(expected, actual)
-    
+        self.assert_daybydays(7)
 
     def test_use_latest_rrule_for_end_date(self):
-        actual = get_daybyday(
+        self.assertEqual("2018-07-22",  get_daybyday(
             ExecutionParameters(
                 date(2018, 6, 20),
                 date(2018, 6, 22),
@@ -126,44 +98,16 @@ class HandlerTests(unittest.TestCase):
                     "value": 100
                 }
             })
-        )
-        self.assertEqual("2018-07-22", actual[-1]["date"])
-
+        )[-1]["date"])
 
     def test_get_daybyday_from_rules_multipleRules(self):
-        inputs = get_inputs(8)
-        actual = get_daybyday(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_daybydays(8)
-        self.assertEqual(expected, actual)
+        self.assert_daybydays(8)
 
     def test_get_daybyday_from_rules_weekly(self):
-        inputs = get_inputs(9)
-        actual = get_daybyday(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_daybydays(9)
-        self.assertEqual(expected, actual)
-
+        self.assert_daybydays(9)
 
     def test_get_daybyday_from_rules_daily(self):
-        inputs = get_inputs(10)
-        actual = get_daybyday(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_daybydays(10)
-        self.assertEqual(expected, actual)
-
+        self.assert_daybydays(10)
 
     def test_get_daybyday_from_rules_monthly(self):
-        inputs = get_inputs(11)
-        actual = get_daybyday(
-            get_parameters_from_inputs(inputs),
-            get_rules_from_inputs(inputs)
-        )
-        expected = get_expected_daybydays(11)
-        self.assertEqual(expected, actual)
+        self.assert_daybydays(11)
