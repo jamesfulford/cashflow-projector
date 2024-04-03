dfea45d99d7623024226fdaed3a2f1f2ba6b3e30
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-04-03 09:48:56 -0600

    fix: remove error when page has updated

--- src/components/AgGrid.tsx
+++ src/components/AgGrid.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { AgGridReact } from "ag-grid-react";
 
-export const AgGrid = React.lazy(async () => {
-  const module = await import("ag-grid-react");
+export const AgGrid = AgGridReact;
 
-  return { default: module.AgGridReact };
-});
+// TODO: lazy-load AgGrid
+// but avoid this error: https://stackoverflow.com/questions/72376333/failed-to-fetch-dynamically-imported-module
+// TypeError: Failed to fetch dynamically imported module: https://jamesfulford.github.io/cashflow-projector/assets/index.esm-CoPhegcv.js\nEnd of stack for Error object
