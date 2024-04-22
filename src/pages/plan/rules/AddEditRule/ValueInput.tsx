8d23a9ea75c15b1d2db89c73ad09929391251e8e
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-04-22 17:34:43 -0600

    fix: color red for expenses

--- src/pages/plan/rules/AddEditRule/ValueInput.tsx
+++ src/pages/plan/rules/AddEditRule/ValueInput.tsx
@@ -39,6 +39,9 @@ export const ValueInput = () => {
                 const newValue = isExpense ? -newRawValue : newRawValue;
                 form.setFieldValue("value", newValue);
               }}
+              style={{
+                color: isExpense ? "var(--red)" : "var(--primary)",
+              }}
               onBlur={() => {}}
             />
             <RequiredInputGroup />
