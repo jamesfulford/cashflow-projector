305b1972723c735a43974c9430482528e99427f4
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-04-28 11:54:20 -0600

    feat: hide/show start criteria (plus required cases)

--- src/pages/plan/rules/AddEditRule/StartSelector.tsx
+++ src/pages/plan/rules/AddEditRule/StartSelector.tsx
@@ -8,6 +8,8 @@ import { RecurringWorkingState, YEARLY_HEBREW } from "./types";
 import { useState } from "react";
 import { useSignalValue } from "../../../../store/useSignalValue";
 import { startDateState } from "../../../../store/parameters";
+import { DateDisplay } from "../../../../components/date/DateDisplay";
+import Tippy from "@tippyjs/react";
 
 function frequencyIsIn(
   freq: RecurringWorkingState["rrule"]["freq"],
@@ -43,96 +45,131 @@ export const StartSelector = () => {
     form.getFieldMeta("rrule.dtstart").value ? StartType.ON : StartType.NOW,
   );
 
+  const freqName =
+    freq === RRule.WEEKLY
+      ? "week"
+      : freq === RRule.MONTHLY
+        ? "month"
+        : freq === RRule.YEARLY
+          ? "year"
+          : "";
+  const freqNamePlural = freqName + "s";
+
+  const requiredByInterval = interval > 1;
+  const requiredByIntervalWhy = `Because interval is greater than 1 (is ${interval}), we are skipping some ${freqNamePlural}. We need to know the first non-skipped ${freqName} so we consistently skip the same ${freqNamePlural}.`;
+  const requiredByYear = frequencyIsIn(freq, [RRule.YEARLY]);
+  const requiredByYearWhy = `Because the frequency is yearly, we need to know which day of the year to use.`;
+  const requiredByEndCount = !!count;
+  const requiredByEndCountWhy = `Because the end occurs after a set number of times (is ${count}), we need to nail down which date to start counting.`;
+  const required = requiredByInterval || requiredByYear || requiredByEndCount;
+
+  const effectiveStartType = required ? StartType.ON : startType;
+
+  const [show, setShow] = useState(false);
+  const effectiveShow = show || required;
+
   return (
     <Field name="rrule.dtstart">
       {({ field }: FieldProps) => {
-        const requiredByInterval = interval > 1;
-        const requiredByYear = frequencyIsIn(freq, [RRule.YEARLY]);
-        const requiredByEndCount = !!count;
-        const required =
-          requiredByInterval || requiredByYear || requiredByEndCount;
-
-        const effectiveStartType = required ? StartType.ON : startType;
-        const freqName =
-          freq === RRule.WEEKLY
-            ? "week"
-            : freq === RRule.MONTHLY
-              ? "month"
-              : freq === RRule.YEARLY
-                ? "year"
-                : "";
-        const freqNamePlural = freqName + "s";
-
-        if (effectiveStartType === StartType.NOW) {
-          return (
+        return (
+          <>
             <Button
+              aria-label="Show start criteria"
               variant="link"
-              className="p-0 m-0 underline-on-hover"
+              className={"p-0 m-0 " + (required ? "" : "underline-on-hover")}
               style={{
                 color: "var(--gray-text)",
                 textDecoration: "none",
+                cursor: required ? "not-allowed" : "pointer",
+                opacity: required ? "80%" : "100%",
               }}
               onClick={() => {
-                setStartType(StartType.ON);
+                if (required) return;
+                setShow((s) => !s);
               }}
             >
-              Starts immediately
+              {effectiveShow ? (
+                required ? (
+                  <Tippy
+                    content={
+                      <>
+                        Required.
+                        <br />
+                        {requiredByInterval && requiredByIntervalWhy}
+                        {requiredByYear && requiredByYearWhy}
+                        {requiredByEndCount && requiredByEndCountWhy}
+                      </>
+                    }
+                  >
+                    <span>Start criteria (required)</span>
+                  </Tippy>
+                ) : (
+                  <>Hide start criteria</>
+                )
+              ) : (
+                <>
+                  Show start criteria (
+                  {field.value ? (
+                    <>
+                      starting on <DateDisplay date={field.value} />
+                    </>
+                  ) : (
+                    <>starting immediately</>
+                  )}
+                  )
+                </>
+              )}
             </Button>
-          );
-        }
-        return (
-          <InputGroup>
-            {required ? (
-              <InputGroup.Text>Starting on</InputGroup.Text>
-            ) : (
-              <InputGroup.Text>Starting</InputGroup.Text>
-            )}
+            {effectiveShow && (
+              <InputGroup className="mt-2 mb-2">
+                {required ? (
+                  <InputGroup.Text>Starting on</InputGroup.Text>
+                ) : (
+                  <InputGroup.Text>Starting</InputGroup.Text>
+                )}
 
-            {!required && (
-              <BSForm.Select
-                aria-label="Select how it starts"
-                value={effectiveStartType}
-                disabled={required}
-                onChange={(e) => {
-                  const newStartType: StartType = e.target.value as StartType;
-                  setStartType(newStartType);
-                  if (newStartType !== startType) {
-                    form.setFieldValue("rrule.dtstart", "");
-                  }
-                }}
-              >
-                <option value={StartType.NOW}>immediately</option>
-                <option value={StartType.ON}>on</option>
-              </BSForm.Select>
-            )}
+                {!required && (
+                  <BSForm.Select
+                    aria-label="Select how it starts"
+                    value={effectiveStartType}
+                    disabled={required}
+                    onChange={(e) => {
+                      const newStartType: StartType = e.target
+                        .value as StartType;
+                      setStartType(newStartType);
+                      if (newStartType !== startType) {
+                        form.setFieldValue("rrule.dtstart", "");
+                      }
+                    }}
+                  >
+                    <option value={StartType.NOW}>immediately</option>
+                    <option value={StartType.ON}>on</option>
+                  </BSForm.Select>
+                )}
 
-            {effectiveStartType === StartType.ON ? (
-              <>
-                <BSForm.Control
-                  type="date"
-                  required={required}
-                  // no min date so can select past dates (or not have to edit previously set past dates)
-                  {...field}
-                  value={field.value ?? startDate} // default to today (avoid React warning when undefined)
-                />
-                {requiredByInterval && (
-                  <RequiredInputGroup
-                    why={`Because interval is greater than 1 (is ${interval}), we are skipping some ${freqNamePlural}. We need to know the first non-skipped ${freqName} so we consistently skip the same ${freqNamePlural}.`}
-                  />
-                )}
-                {requiredByYear && (
-                  <RequiredInputGroup
-                    why={`Because the frequency is yearly, we need to know which day of the year to use.`}
-                  />
-                )}
-                {requiredByEndCount && (
-                  <RequiredInputGroup
-                    why={`Because the end occurs after a set number of times (is ${count}), we need to nail down which date to start counting.`}
-                  />
-                )}
-              </>
-            ) : null}
-          </InputGroup>
+                {effectiveStartType === StartType.ON ? (
+                  <>
+                    <BSForm.Control
+                      type="date"
+                      required={required}
+                      // no min date so can select past dates (or not have to edit previously set past dates)
+                      {...field}
+                      value={field.value ?? startDate} // default to today (avoid React warning when undefined)
+                    />
+                    {requiredByInterval && (
+                      <RequiredInputGroup why={requiredByIntervalWhy} />
+                    )}
+                    {requiredByYear && (
+                      <RequiredInputGroup why={requiredByYearWhy} />
+                    )}
+                    {requiredByEndCount && (
+                      <RequiredInputGroup why={requiredByEndCountWhy} />
+                    )}
+                  </>
+                ) : null}
+              </InputGroup>
+            )}
+          </>
         );
       }}
     </Field>
