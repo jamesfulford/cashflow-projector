a5a879547966cdf67c669a84304e48fd074c23bd
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-04-28 13:48:42 -0600

    feat: hide/show end criteria

--- src/pages/plan/rules/AddEditRule/EndSelector.tsx
+++ src/pages/plan/rules/AddEditRule/EndSelector.tsx
@@ -4,6 +4,7 @@ import InputGroup from "react-bootstrap/esm/InputGroup";
 import BSForm from "react-bootstrap/esm/Form";
 import { useFormikContext } from "formik";
 import { useState } from "react";
+import { DateDisplay } from "../../../../components/date/DateDisplay";
 
 enum EndType {
   NEVER = "NEVER",
@@ -14,89 +15,103 @@ enum EndType {
 export const EndSelector = () => {
   const form = useFormikContext();
 
+  const until = form.getFieldMeta("rrule.until").value as string | undefined;
+  const count = form.getFieldMeta("rrule.count").value as number | undefined;
+
   const [endType, setEndType] = useState<EndType>(
-    form.getFieldMeta("rrule.until").value
-      ? EndType.ON
-      : form.getFieldMeta("rrule.count").value
-        ? EndType.AFTER
-        : EndType.NEVER,
+    until ? EndType.ON : count ? EndType.AFTER : EndType.NEVER,
   );
+
+  const [show, setShow] = useState(false);
 
   return (
     <>
-      {endType === EndType.NEVER ? (
-        <>
-          <Button
-            variant="link"
-            className="p-0 m-0 underline-on-hover"
-            style={{
-              color: "var(--gray-text)",
-              textDecoration: "none",
-            }}
-            onClick={() => {
-              setEndType(EndType.ON);
+      <Button
+        variant="link"
+        className="p-0 m-0 underline-on-hover"
+        style={{
+          color: "var(--gray-text)",
+          textDecoration: "none",
+        }}
+        onClick={() => {
+          setShow((s) => !s);
+        }}
+      >
+        {show ? (
+          <>Hide ending criteria</>
+        ) : (
+          <>
+            Show ending criteria (
+            {until ? (
+              <>
+                ending on <DateDisplay date={until} />
+              </>
+            ) : count ? (
+              <>
+                ending after {count} {count > 1 ? "times" : "time"}
+              </>
+            ) : (
+              <>never ending</>
+            )}
+            )
+          </>
+        )}
+      </Button>
+      {show && (
+        <InputGroup className="mt-2 mb-2">
+          <InputGroup.Text>Ending</InputGroup.Text>
+          <BSForm.Select
+            aria-label="Select how it ends"
+            value={endType}
+            onChange={(e) => {
+              const newEndType = e.target.value as EndType;
+              setEndType(newEndType);
+              if (newEndType !== endType) {
+                form.setFieldValue("rrule.until", "");
+                form.setFieldValue(
+                  "rrule.count",
+                  newEndType === EndType.AFTER ? 10 : 0,
+                ); // if 0, translates to undefined
+              }
             }}
           >
-            Never ends
-          </Button>
-        </>
-      ) : (
-        <>
-          <InputGroup>
-            <InputGroup.Text>Ending</InputGroup.Text>
-            <BSForm.Select
-              aria-label="Select how it ends"
-              value={endType}
-              onChange={(e) => {
-                const newEndType = e.target.value as EndType;
-                setEndType(newEndType);
-                if (newEndType !== endType) {
-                  form.setFieldValue("rrule.until", "");
-                  form.setFieldValue(
-                    "rrule.count",
-                    newEndType === EndType.AFTER ? 10 : 0,
-                  ); // if 0, translates to undefined
-                }
+            <option value={EndType.NEVER}>never</option>
+            <option value={EndType.ON}>on</option>
+            <option value={EndType.AFTER}>after</option>
+          </BSForm.Select>
+          {endType === EndType.ON ? (
+            <Field name="rrule.until">
+              {({ field }: FieldProps) => {
+                return (
+                  <BSForm.Control
+                    type="date"
+                    min={form.getFieldMeta("rrule.dtstart").value as string}
+                    {...field}
+                  />
+                );
               }}
-            >
-              <option value={EndType.NEVER}>never</option>
-              <option value={EndType.ON}>on</option>
-              <option value={EndType.AFTER}>after</option>
-            </BSForm.Select>
-            {endType === EndType.ON ? (
-              <Field name="rrule.until">
-                {({ field }: FieldProps) => {
-                  return (
+            </Field>
+          ) : null}
+          {endType === EndType.AFTER ? (
+            <Field name="rrule.count">
+              {({ field }: FieldProps) => {
+                return (
+                  <>
                     <BSForm.Control
-                      type="date"
-                      min={form.getFieldMeta("rrule.dtstart").value as string}
+                      placeholder="After"
+                      type="number"
+                      min={1}
                       {...field}
                     />
-                  );
-                }}
-              </Field>
-            ) : null}
-            {endType === EndType.AFTER ? (
-              <Field name="rrule.count">
-                {({ field }: FieldProps) => {
-                  return (
-                    <>
-                      <BSForm.Control
-                        placeholder="After"
-                        type="number"
-                        min={1}
-                        {...field}
-                      />
-                      <InputGroup.Text>
-                        {field.value > 1 ? "times" : "time"}
-                      </InputGroup.Text>
-                    </>
-                  );
-                }}
-              </Field>
-            ) : null}
-          </InputGroup>
-        </>
+                    <InputGroup.Text>
+                      {field.value > 1 ? "times" : "time"}
+                    </InputGroup.Text>
+                  </>
+                );
+              }}
+            </Field>
+          ) : null}
+        </InputGroup>
       )}
     </>
   );
