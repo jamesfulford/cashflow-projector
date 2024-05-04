13f7960f2c59b563e349ab388a7ecb97d376c918
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-05-03 18:17:12 -0600

    feat: if nofs, don't say 'Select save file'

--- src/components/SaveIndicator.tsx
+++ src/components/SaveIndicator.tsx
@@ -40,18 +40,24 @@ export function SaveIndicator() {
     return null; // "New" case; don't show in UI yet
 
   // No file handle; request save
-  if (fileSyncState === ProfileSaveNeededState.NO_SYNC)
-    return <PureSaveIndicator message="Select save file*" />;
 
-  // autosaves immediately; no need to flash Saved/Unsaved message
-  if (isFilesystemSupported) return <PureSaveIndicator message="Autosaved" />;
+  if (isFilesystemSupported) {
+    if (fileSyncState === ProfileSaveNeededState.NO_SYNC)
+      return <PureSaveIndicator message="Select save file*" />;
+
+    // autosaves immediately; no need to flash Saved/Unsaved message
+    if (isFilesystemSupported) return <PureSaveIndicator message="Autosaved" />;
+  }
 
   // File handle exists and changes saved
   if (fileSyncState === ProfileSaveNeededState.IN_SYNC)
     return <PureSaveIndicator message="Saved" />;
 
   // File handle exists but changes not saved (no autosave enabled)
-  if (fileSyncState === ProfileSaveNeededState.OUT_OF_SYNC)
+  if (
+    fileSyncState === ProfileSaveNeededState.OUT_OF_SYNC ||
+    fileSyncState === ProfileSaveNeededState.NO_SYNC
+  )
     return <PureSaveIndicator message="Unsaved changes*" />;
 
   // should never happen
