7771a4bbb31efced6b527e8b9e83dd015b5fa991
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-05-24 08:52:37 -0600

    remove PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
-const plugins = [
-  react(),
-  VitePWA({
-    registerType: "autoUpdate",
-    workbox: {
-      maximumFileSizeToCacheInBytes: 2097152 * 2,
-    },
-  }),
-];
+const plugins = [react()];
 
 export default defineConfig({
   build: {
