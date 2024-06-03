8fc5db372f74bdeaeea96970040637ddcf556112
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-06-03 11:24:15 -0600

    feat: Vercel SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import posthog from "posthog-js";
 import "./main.scss";
 import { TooltipSingletonProvider } from "./components/Tooltip.tsx";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 posthog.init("phc_Vgmc1Gq9hqRmpV8GKsjEJXLGvVflRjO4SOBnDUrp8BD", {
   api_host: "https://app.posthog.com",
   opt_in_site_apps: true,
@@ -28,5 +30,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <TooltipSingletonProvider>
       <App />
     </TooltipSingletonProvider>
+    <SpeedInsights />
   </React.StrictMode>,
 );
