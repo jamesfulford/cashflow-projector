a1db53dfade26db1632f438a29778c2e2446920d
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-06-03 14:51:04 -0600

    feat: split vendor chunks, remove warning

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,20 @@ const plugins = [react()];
 export default defineConfig({
   build: {
     target: "es2021",
+    chunkSizeWarningLimit: 991, // ag-grid-community
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // split each dependency (ish) into separate chunks for great caching even across updates
+          if (id.includes("node_modules")) {
+            const moduleName = (id.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+            ) || [])[1];
+            return moduleName.replace("@", "");
+          }
+        },
+      },
+    },
   },
   test: {
     environment: "jsdom",
